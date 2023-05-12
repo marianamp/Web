d8073e0762446d0c56255ab504de79194dd97da6
marianamp <49280457+marianamp@users.noreply.github.com>
2023-05-12 19:02:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function redirect_home(){
+    window.location.href = "dashboard.html";
+}
+function redirect_register(){
+    window.location.href = "index.html";
+}
+function redirect_login(){
+    window.location.href = "login.html";
+}
